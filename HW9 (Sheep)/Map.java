4272b271527f1257e6584e36a98a5f9eb0fc3133
Austin Fernandez <ryanaustinf@yahoo.com>
2016-03-23 09:03:02 +0800

    SHEEEEEEEEEEEEEEEEP!!!!!!!!!!!!!!!!!!!!!!!
    SHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEP!!!!!!!!!

---
+++ HW9 (Sheep)/Map.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+
+public class Map {
+	private Tile[][] map;
+	private ArrayList<Sheep> sheep;
+
+	public Map(int dimension) {
+		map = new Tile[dimension][dimension];
+		for(int i = 0; i < dimension; i++) {
+			for( int j = 0; j < dimension; j++ ) {
+				map[i][j] = new Tile();
+			}
+		}
+		sheep = new ArrayList<Sheep>();
+	}
+
+	public synchronized String spawnSheep() {
+		int id = sheep.size();
+		int x = (int)(Math.random() * map.length);
+		int y = (int)(Math.random() * map.length);
+		Sheep s = new Sheep(id,x,y);
+		map[y][x].addSheep(s);
+		sheep.add(s);
+		return s.id() + "";
+	}
+
+	public void moveSheep(String id, String dir) {
+		Sheep s = sheep.get(Integer.parseInt(id));
+		switch(dir) {
+			case "U":
+				if( s.y() > 0 ) {
+					map[s.y()][s.x()].removeSheep(s);
+					s.move(dir);
+					map[s.y()][s.x()].addSheep(s);
+				}
+				break;
+			case "L":
+				if( s.x() > 0 ) {
+					map[s.y()][s.x()].removeSheep(s);
+					s.move(dir);
+					map[s.y()][s.x()].addSheep(s);
+				}
+				break;
+			case "D":
+				if( s.y() < map.length - 1 ) {
+					map[s.y()][s.x()].removeSheep(s);
+					s.move(dir);
+					map[s.y()][s.x()].addSheep(s);
+				}
+				break;
+			case "R":
+				if( s.x() < map.length - 1 ) {
+					map[s.y()][s.x()].removeSheep(s);
+					s.move(dir);
+					map[s.y()][s.x()].addSheep(s);
+				}
+				break;
+			default:	
+		}
+	}
+
+	public synchronized boolean done() {
+		for(int i = 0; i < map.length; i++) {
+			for(int j = 0; j < map.length; j++ ) {
+				if( map[i][j].hasGrass() ) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public synchronized boolean eat(String id) {
+		Sheep s = sheep.get(Integer.parseInt(id));
+		if( map[s.y()][s.x()].eat() ) {
+			s.eat();
+		}
+		return done();
+	}
+
+	public synchronized String sheep() {
+		String ret = "[";
+		for(int i = 0; i < sheep.size(); i++) {
+			if( i > 0 ) {
+				ret += ",";
+			}
+			ret += sheep.get(i).toString();
+		}
+		return ret + "]";
+	}
+
+	public String toString() {
+		String ret = "";
+		for(int i = 0; i < map.length; i++) {
+			if( i > 0 ) {
+				ret += "\\n";
+			}
+			for(int j = 0; j < map.length; j++ ) {
+				ret += map[i][j].toString();
+			}
+		}
+		return ret;
+	}
+}
---
+++ HW9 (Sheep)/Server.java
@@ -0,0 +1,279 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.EOFException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Server {
+	private ServerSocket ss;
+	private ProcessThread[] processors;
+	private Map field;
+
+	public static void main(String[] args) {
+		Server s = new Server();
+		s.start();
+	}
+
+	public Server() {
+		try {
+			ss = new ServerSocket(8081);
+			processors = new ProcessThread[20];
+			for(int i = 0; i < processors.length; i++ ) {
+				processors[i] = new ProcessThread();
+				processors[i].start();
+			}
+			field = new Map(20);
+ 		} catch( IOException ioe ) {
+			ioe.printStackTrace();
+		}
+	}
+
+	public void start() {
+		Socket curr;
+		int i = 0;
+		while(true) {
+			try {
+				System.out.println("Waiting");
+				curr = ss.accept();
+				System.out.println("CONNECTED TO " + curr.getInetAddress().getHostAddress());
+				processors[i].addSocket(curr);
+				i = (i + 1) % processors.length;
+			} catch(IOException ioe) {
+				ioe.printStackTrace();
+			}
+		}
+	}
+
+	class ProcessThread extends Thread {
+		private Stack<Socket> sockets;
+
+		public ProcessThread() {
+			sockets = new Stack<Socket>();
+		}
+
+		public synchronized void addSocket(Socket s) {
+			sockets.push(s);
+			notifyAll();
+		}
+
+		public synchronized void tryProcess() {
+			while(sockets.size() == 0 ) {
+				try {
+					wait();
+				} catch( Exception e) {
+					e.printStackTrace();
+				}
+			}
+			Socket s = sockets.pop();
+			process(s);
+		}
+
+		public void run() {
+			while(true) {
+				tryProcess();
+			}
+		}
+
+		public void process(Socket curr) {
+			try {
+				String ip = curr.getInetAddress().getHostAddress();
+				System.out.println("PROCESSING " + ip);
+				DataInputStream dis = new DataInputStream(curr.getInputStream());
+				String message = "";
+				String post = "";
+				char c;
+				int end = 0;
+				String type = "";
+				HashMap<String,String> params = new HashMap<String,String>();
+				do {
+					try {
+						c = (char)dis.readUnsignedByte();
+					} catch(EOFException e) {
+						break;
+					}
+					switch( end ) {
+						case 0:
+							if( c == '\r') {
+								end++;
+							}
+							break;
+						case 1:
+							if( c == '\n') {
+								end++;
+							} else {
+								end = 0;
+							}
+							break;
+						case 2:
+							if( c == '\r') {
+								end++;
+							} else {
+								end = 0;
+							}
+							break;
+						case 3:
+							if( c == '\n') {
+								end++;
+							} else {
+								end = 0;
+							}
+							break;
+						default:	
+					}
+					message += c;
+					if( end == 4 ) {
+						System.out.println("THIS IS THE MESSAGE: " + message);
+						if(message.split(" ")[0].equals("POST")) {
+							type = "POST";
+							String[] parts = message.split("\n");
+							int length = 0;
+							for(String s: parts) {
+								if( s.startsWith("Content-Length: ")) {
+									length 
+										= Integer.parseInt(
+												s.substring("Content-Length: "
+																.length()
+															,s.length() - 1));
+									break;
+								}
+							}
+							for(int i = 0; i < length; i++) {
+								post += (char)dis.readUnsignedByte();
+							}
+							post = URLDecoder.decode(post,"UTF-8");
+						} else {
+							type = "GET";
+						}
+					} 
+				} while( end < 4 );
+				try {
+					if( message.length() > 0 ) {
+						String fn = message.split(" ")[1].substring(1);
+
+						String[] parts = fn.split("\\?");
+						fn = parts[0];
+						String[] paramsStr = new String[0];
+						if( post.length() > 0 ) {
+							paramsStr = post.split("&");
+						} else if( parts.length > 1 ) {
+							paramsStr = parts[1].split("&");
+						}
+
+						for( String s: paramsStr) {
+							String[] par = s.split("=");
+							params.put(par[0],par.length == 1 ? "" : par[1]);
+							System.out.println("putting " + par[0] + " -> " 
+												+ (par.length == 1 ? "" : par[1]));
+						}
+
+						String replyContent = "";
+
+						switch(type) {
+							case "GET":
+								if(!fn.equals("sheep")) {
+									try {
+										int length = 32768;
+										File file = new File(fn);
+										DataInputStream is = new DataInputStream(
+																	new FileInputStream(file));
+										byte[] fileInput = new byte[length];
+										int current = 0;
+										int read;
+
+										do{
+											read = is.read(fileInput,0
+															,(int)Math.min(length
+																,file.length() - current));
+											if( read >= 0 ) {
+												current += read;
+											}
+											replyContent += (new String(fileInput))
+																.substring(0,read);
+										} while(current < file.length());
+									} catch( IOException ioe ) {
+
+									}
+								} else {
+									replyContent = "{\"id\":\"" + field.spawnSheep() 
+											+ "\",\"map\":\"" + field.toString() 
+											+ "\",\"sheep\":" + field.sheep() + "}";
+								}
+								break;
+							case "POST":
+								String dir = params.get("dir");
+								String id = params.get("id");
+										
+								switch(params.get("request")) {
+									case "move":
+										field.moveSheep(id,dir);
+										break;
+									case "eat":
+										boolean done = field.eat(id);
+										break;
+									default:
+								}
+								replyContent = "{\"map\":\"" + field.toString() 
+											+ "\",\"sheep\":" + field.sheep() 
+											+ ",\"done\":\"" + field.done() + "\"}";
+								break;
+							default:
+						}
+
+						DataOutputStream dos 
+							= new DataOutputStream(curr.getOutputStream());
+						String reply = "HTTP/1.1 200 OK\r\n" + 
+										"Date: Mon, 23 May 2005 22:38:34 GMT\r\n" + 
+										"Content-Type: text/html; charset=UTF-8\r\n" + 
+										"Content-Encoding: UTF-8\r\n";
+						
+						reply += "Content-Length: " + replyContent.length() 
+									+ "\r\n" + 
+									"Server: Java/1.0 (Unix)\r\n" + 
+									"ETag: \"3f80f-1b6-3e1cb03b\"\r\n" + 
+									"Accept-Ranges: bytes\r\n" + 
+									"Connection: close\r\n\r\n" + replyContent;
+						// System.out.println(reply);
+						dos.writeBytes(reply);
+						dos.flush();
+						dos.close();
+					} else {
+						DataOutputStream dos 
+							= new DataOutputStream(curr.getOutputStream());
+						// System.out.println(reply);
+						dos.writeBytes("HTTP/1.1 200 OK\r\n" + 
+									"Date: Mon, 23 May 2005 22:38:34 GMT\r\n" + 
+									"Content-Type: text/html; charset=UTF-8\r\n" + 
+									"Content-Encoding: UTF-8\r\n" + 
+									"Content-Length: 0\r\n" + 
+									"Server: Java/1.0 (Unix)\r\n" + 
+									"ETag: \"3f80f-1b6-3e1cb03b\"\r\n" + 
+									"Accept-Ranges: bytes\r\n" + 
+									"Connection: close\r\n\r\n"
+									);
+						dos.flush();
+						dos.close();
+					}
+				} catch(IOException ioe) {}
+				
+				curr.close();
+			} catch( IOException ioe ) {
+				ioe.printStackTrace();
+			}
+		}
+	}
+}
